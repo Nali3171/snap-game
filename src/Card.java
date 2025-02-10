c23ee34d912dc0f212425d2d811c0534255bbcee
nazrin ali <nali3171@outlook.com>
2025-02-10 16:15:19 +0000

    created string, and int inside card class and created a constuctor

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,28 @@
 public class Card {
-}
+        String suit;
+        String symbol;
+        int value;
+
+        public Card(String suit, String symbol, int value){
+            this.suit = suit;
+            this.symbol = symbol;
+            this.value = value;
+        }
+        // Add getters here:
+    public String getSuit(){
+            return suit;
+        }
+        public String getSymbol(){
+            return symbol;
+        }
+        public int getValue(){
+            return value;
+        }
+
+        //add a method called toString()
+      //  public String toString(){
+     //       return symbol + " of "+ suit;
+      //  }
+
+    }
+
--- src/main.java
+++ src/main.java
@@ -1,2 +1,7 @@
 public class main {
+    public static void main(String[] args) {
+        Card card = new Card(" Heart", "A", 14);
+        System.out.println(card);
+
+    }
 }
