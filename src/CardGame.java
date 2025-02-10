25efd3098ce7bdb402603ea529a9c2561fd9edf7
nazrin ali <nali3171@outlook.com>
2025-02-10 16:33:37 +0000

    create an array list with constructor

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 public class CardGame {
+private String cardname;
+private ArrayList<Card> deck;
+
+//private static final String[] Suits = {" Heart ", " Spade ", " Diamond ", " Clubs"}
 
 
+    //constructor
+    public CardGame(String name){
+        this.cardname = cardname;
+        this.deck = new ArrayList<>();
+        getDeck();
+    }
+    // method for get deck
+    private void getDeck(){
+
+    }
 }
