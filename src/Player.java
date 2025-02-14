8c241d5739696029d321b2f02f59231a4667fede
nazrin ali <nali3171@outlook.com>
2025-02-14 15:45:45 +0000

    Use players class to allow cards to be taken and placed from player in game

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,26 @@
+import javax.swing.plaf.PanelUI;
 import java.util.ArrayList;
 
 public class Player {
     private String name;
     private ArrayList<Card> hand;
+
+    public Player(String name){
+        this.name = name;
+        this.hand = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addCardToHandOfPlayer(Card card){
+        hand.add(card);
+    }
+    public Card removeCardFromHand() {
+        if (!hand.isEmpty()) {
+            return hand.removeFirst(); // Remove the first card from the hand
+        }
+        return null;
+    }
 }
