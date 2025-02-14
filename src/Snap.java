0d80fde2b6cbfae7f77837a4fc34b593bb88f0b0
nazrin ali <nali3171@outlook.com>
2025-02-14 15:30:05 +0000

    created snap class whch extends cardGame and set up properties

--- src/Snap.java
+++ src/Snap.java
@@ -1,2 +1,18 @@
-public class Snap {
+
+
+public class Snap extends CardGame{
+
+    private Player player1;
+    private Player player2;
+    private Player previousCard;
+
+
+    public Snap(String snap){
+        super(snap);
+        this.player1 = new Player("Player 1");
+        this.player2= new Player("Player 2")
+    }
+
+
+    //beginning of game
 }
