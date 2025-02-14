08262771afdf092607f4b31a1a17cfa374eced60
nazrin ali <nali3171@outlook.com>
2025-02-14 14:36:32 +0000

    created dealcard method

--- src/CardGame.java
+++ src/CardGame.java
@@ -5,27 +5,37 @@ public class CardGame {
     private String name;
 
 
+
+    private static final String[] Suits = {"♥", "♠", "♦", "♣"}; //Array for suit
+    private static final String[] Symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}; //array for symbol
+
     //constructor
     public CardGame(String name) {
         this.deckOfCards = new ArrayList<>();
         this.name = name;
     }
 
-    private static final String[] Suits = {"♥", "♠", "♦", "♣"}; //Array for suit
-    private static final String[] Symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}; //array for symbol
-
-
 //Make methods
 
 //method for making deck
     private void createDeck(){
         for (String suit : Suits){
-            for(i = 0; i < Symbols.length; i++){
+            for(int i = 0; i < Symbols.length; i++){
                int value = i + 2;
-               deckOfCards.add(new Card(suit,Symbols[i], value));
+               deckOfCards.add(String.valueOf(new Card(suit,Symbols[i], value)));
             }
         }
     }
+
+    //method for dealing card from top of deck
+    public String dealCard(){
+        if (deckOfCards.isEmpty()){
+            System.out.println("No more cards to deal");
+            return null;
+        }
+        return deckOfCards.removeFirst();
+    }
+
 
 
 //method for getDeck()
