5265f2be5cecc2a3700f629860a3661bcb50a8de
nazrin ali <nali3171@outlook.com>
2025-02-17 16:22:16 +0000

    added a timer

--- src/Snap.java
+++ src/Snap.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Snap extends CardGame{
 
@@ -30,8 +32,13 @@ public class Snap extends CardGame{
 
             // Check if there's a snap
             if (checkSnap()) {
+                snapTimer(scanner, player1, player2);
                 System.out.println("SNAP! " + player1.getName() + " wins!");
                 return;
+            }
+            if (deckOfCards.isEmpty()) {
+                System.out.println("we have no cards left, Game over!");
+                break;
             }
 
             // Switch to Player 2
@@ -41,10 +48,15 @@ public class Snap extends CardGame{
             takeTurn(player2);
 
             if (checkSnap()) {
-                System.out.println("SNAP! " + player2.getName() + " wins!");
+                snapTimer(scanner, player2, player1);
                 return;
-            }//switching between player, bulk of game
-        }    System.out.println("Game over!");
+            }
+            if (deckOfCards.isEmpty()) {
+                System.out.println("we have no cards left, Game over!");
+                break;
+            }
+            //switching between player, bulk of game
+        }   System.out.println("Game over!");
     }//game over
 
     private void takeTurn(Player player) {
@@ -58,6 +70,39 @@ public class Snap extends CardGame{
     private boolean checkSnap() {
         return previousCard != null && deckOfCards.size() > 0 &&
                 previousCard.getSymbol().equals(deckOfCards.getFirst().getSymbol());
+    }
+
+    //implement 2 second timer
+    private boolean snapCalled;
+    private void snapTimer(Scanner scanner, Player currentPlayer, Player otherPlayer){
+        snapCalled = false;
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(!snapCalled) {
+                    System.out.println(otherPlayer.getName() + " gets a chance to type 'snap'.");
+                    System.out.println(otherPlayer.getName() + ", type 'snap' to win!");
+
+                    String input = scanner.nextLine();
+                    if (input.equalsIgnoreCase("snap")) {
+                        System.out.println(otherPlayer.getName() + " wins the game!");
+                    } else {
+                        System.out.println("No snap. Game continues!");
+                    }
+                }
+                    timer.cancel();
+                }
+            },2000);
+
+            String input = scanner.nextLine();
+        if(input.equals("snap")){
+            snapCalled= true;
+                System.out.println(currentPlayer.getName() + " wins the game");
+                timer.cancel();
+        }
     }
 
     public void snap(Player player) {
